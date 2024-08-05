e082b50c2c6cf8d3cb3d5706a0178ee997948dc3
Gonzalo Bermúdez <gonzo@yopventures.com>
2024-08-05 14:24:12 +0200

    fix version format

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024041608.08;       // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2024041608;       // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100400;    // Requires this Moodle version.
 $plugin->component = 'mod_pokcertificate';       // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
